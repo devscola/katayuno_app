91af5c7a0bc93f67467625b4f0eb7f9fc2c23968
zerolive <zerolive@gmail.com>
2017-11-06 12:34:48 +0100

    User can do log out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def session_actions
+    return log_out_link if user_signed_in?
+
+    log_in_link
+  end
+
+  def logged_user
+    current_user.email if user_signed_in?
+  end
+
+  private
+
+  def log_out_link
+    text = t(:log_out)
+    link_to(text, destroy_user_session_path, method: :delete)
+  end
+
+  def log_in_link
+    text = t(:log_in)
+    link_to(text, new_user_session_path)
+  end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,23 +2,42 @@ require 'rails_helper'
 require 'capybara'
 
 describe 'User' do
-  it 'can do log in' do
-    user_email = 'user@test.com'
-    user_password = '12345678'
-    user = User.new(
-      email: user_email,
-      password: user_password,
-      password_confirmation: user_password
+  before do
+    @user = User.new(
+      email: 'user@test.com',
+      password: '12345678',
+      password_confirmation: '12345678'
     )
-    user.save
+    @user.save
+  end
 
+  it 'can do log in' do
     visit root_path
     click_on('Log in')
-    fill_in(:user_email, with: user_email)
-    fill_in(:user_password, with: user_password)
-    find('input[name="commit"]').click
+    fill_in(:user_email, with: @user.email)
+    fill_in(:user_password, with: @user.password)
+
+    click_on_log_in_form
 
     expect(page).to have_content('Log out')
-    expect(page).to have_content(user_email)
+    expect(page).to have_content(@user.email)
+  end
+
+  it 'can do log out' do
+    visit root_path
+    click_on('Log in')
+    fill_in(:user_email, with: @user.email)
+    fill_in(:user_password, with: @user.password)
+    click_on_log_in_form
+
+    click_on('Log out')
+
+    expect(page).to have_content('Log in')
+    expect(page).not_to have_content(@user.email)
+    expect(page).not_to have_content('Log out')
+  end
+
+  def click_on_log_in_form
+    find('input[name="commit"]').click
   end
 end
