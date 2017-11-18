cfc65fb830f29567299993c94a305a8aaec19dd9
zerolive <zerolive@gmail.com>
2017-11-18 21:07:40 +0100

    Example has to have a text and url

--- app/models/example.rb
+++ app/models/example.rb
@@ -1,6 +1,8 @@
 
 class Example < ApplicationRecord
   validates :kata, presence: true, allow_blank: false
+  validates :text, presence: true, allow_blank: false
+  validates :url, presence: true, allow_blank: false
 
   def self.for(kata)
     where(kata: kata)
--- spec/models/examples_spec.rb
+++ spec/models/examples_spec.rb
@@ -13,4 +13,19 @@ describe 'Example' do
     expect(example.errors.size).to eq(1)
     expect(example.errors[:kata]).to eq(["can't be blank"])
   end
+
+  it 'has to have a text and url' do
+    any_kata = 1
+    example = Example.new(
+      text: nil,
+      url: nil,
+      kata: any_kata
+    )
+
+    example.save
+
+    expect(example.errors.size).to eq(2)
+    expect(example.errors[:text]).to eq(["can't be blank"])
+    expect(example.errors[:url]).to eq(["can't be blank"])
+  end
 end
