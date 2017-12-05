bb91bb7b829ce8d2caa8166f12c850fd023ef460
zerolive <zerolive@gmail.com>
2017-12-06 00:07:54 +0100

    Apply DRY on External Examples controller

--- app/controllers/external_examples_controller.rb
+++ app/controllers/external_examples_controller.rb
@@ -1,4 +1,7 @@
 class ExternalExamplesController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :set_example, only: [:edit, :update, :destroy]
+
   def index
     @kata = ExternalKata.find(params[:kata_id])
     @examples = ExternalExample.for(params[:kata_id])
@@ -25,22 +28,25 @@ class ExternalExamplesController < ApplicationController
   end
 
   def edit
-    @example = ExternalExample.find(params[:id])
   end
 
   def update
-    example = ExternalExample.find(params[:id])
-    example.text = params[:external_example][:text]
-    example.url = params[:external_example][:url]
-    example.save
+    @example.text = params[:external_example][:text]
+    @example.url = params[:external_example][:url]
+    @example.save
 
-    redirect_to external_examples_path(example.kata)
+    redirect_to external_examples_path(@example.kata)
   end
 
   def destroy
-    example = ExternalExample.find(params[:id])
-    example.destroy
+    @example.destroy
 
     redirect_to external_examples_path(params[:kata_id])
+  end
+
+  private
+
+  def set_example
+    @example = ExternalExample.find(params[:id])
   end
 end
