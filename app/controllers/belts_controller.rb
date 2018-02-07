3b2c69e7fcfb95346d45e7e30ed5c4aab7ec543d
zerolive <zerolive@gmail.com>
2018-02-07 14:23:44 +0100

    Set belt before some action

--- app/controllers/belts_controller.rb
+++ app/controllers/belts_controller.rb
@@ -1,37 +1,43 @@
 class BeltsController < ApplicationController
   before_action :authenticate_admin!, only: [:create, :edit, :update, :destroy]
+  before_action :set_belt, only: [:edit, :update, :destroy]
 
   def create
-    belt = Belt.new(
+    @belt = Belt.new(
       name: params[:name],
       description: params[:description],
       image: params[:image],
       kata: params[:kata_id]
     )
-    belt.save
+    @belt.save
 
     redirect_to kata_path(params[:kata_id])
   end
 
   def edit
-    @belt = Belt.find(params[:id])
   end
 
   def update
-    belt = Belt.find(params[:id])
-    belt.name = params[:name]
-    belt.description = params[:description]
-    belt.image = params[:image]
-    belt.save
+    @belt.update(
+      name: params[:name],
+      description: params[:description],
+      image: params[:image]
+    )
 
-    redirect_to kata_path(belt.kata)
+    redirect_to kata_path(@belt.kata)
   end
 
   def destroy
-    belt = Belt.find(params[:id])
-    kata_id = belt.kata
+    kata_id = @belt.kata
 
-    belt.destroy
+    @belt.destroy
+
     redirect_to kata_path(kata_id)
+  end
+
+  private
+
+  def set_belt
+    @belt = Belt.find(params[:id])
   end
 end
