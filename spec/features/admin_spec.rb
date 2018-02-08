d895ec2a1c9b47d0ad3a8ab7728ac62adafd5a0f
zerolive <zerolive@gmail.com>
2018-02-08 18:04:31 +0100

    Test admin users for non admins

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -3,44 +3,74 @@ require 'rails_helper'
 require 'capybara'
 
 describe 'Admin' do
-  before(:each) do
-    log_in_admin
-    @user = create_user
+  context 'when an admin is logged' do
+    before(:each) do
+      log_in_admin
+      @user = create_user
+    end
+
+    it 'can view a list of users' do
+
+      click_on(admin_users_text)
+
+      expect(page).to have_content('user@test.com')
+      expect(page).to have_content('Become Admin')
+      expect(page).to have_content('Delete')
+      expect(page).to have_content('admin@test.com')
+      expect(page).to have_content('Revoke Admin')
+    end
+
+    it 'can promote a user to admin' do
+
+      click_on(admin_users_text)
+      click_on('Become Admin')
+
+      expect(page).not_to have_content('Become Admin')
+    end
+
+    it 'can revoke admin to an admin' do
+
+      click_on(admin_users_text)
+      click_on('Revoke Admin')
+
+      expect(page).not_to have_content('Revoke Admin')
+    end
+
+    it 'can delete an user' do
+      url = "/users/#{@user.id}"
+
+      click_on(admin_users_text)
+      find(:xpath, "//a[@href='#{url}']").click
+
+      expect(page).not_to have_content(@user.email)
+    end
   end
 
-  it 'can view a list of users' do
+  context 'when user is not logged' do
+    it 'cannot admin users' do
 
-    click_on('Admin Users')
+      visit '/'
 
-    expect(page).to have_content('user@test.com')
-    expect(page).to have_content('Become Admin')
-    expect(page).to have_content('Delete')
-    expect(page).to have_content('admin@test.com')
-    expect(page).to have_content('Revoke Admin')
+      expect(page).to have_content('Kata-logue')
+      expect(page).not_to have_content(admin_users_text)
+    end
   end
 
-  it 'can promote a user to admin' do
+  context 'when a user is logged' do
+    before do
+      log_in_user
+    end
 
-    click_on('Admin Users')
-    click_on('Become Admin')
+    it 'cannot admin users' do
 
-    expect(page).not_to have_content('Become Admin')
+      visit '/'
+
+      expect(page).to have_content('Kata-logue')
+      expect(page).not_to have_content(admin_users_text)
+    end
   end
 
-  it 'can revoke admin to an admin' do
-
-    click_on('Admin Users')
-    click_on('Revoke Admin')
-
-    expect(page).not_to have_content('Revoke Admin')
-  end
-
-  it 'can delete an user' do
-    url = "/users/#{@user.id}"
-
-    click_on('Admin Users')
-    find(:xpath, "//a[@href='#{url}']").click
-
-    expect(page).not_to have_content(@user.email)
+  def admin_users_text
+    I18n.t(:admin_users)
   end
 end
